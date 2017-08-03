0dcb88dac91f8c4c4331b64832ef5f2cbb30010a
BogdanMorariu <30649415+BogdanMorariu@users.noreply.github.com>
2017-08-03 12:29:04 +0300

    Delete createEvent.sql

--- createEvent.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS "event";
-CREATE TABLE "event" (
-  "id" int PRIMARY KEY,
-  "name" VARCHAR(250) DEFAULT NULL,
-  "location" VARCHAR(250) DEFAULT NULL,
-  "startDate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'EET'),
-  "endDate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'EET'), 
-  "image" bytea
-);
