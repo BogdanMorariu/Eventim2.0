9f0316b3087b0ab2ccd1482ea2d48f72b88ed229
Alex <Alex@10.227.25.106>
2017-08-07 14:46:28 +0300

    commit DAO&Service - Category 1.0

---
+++ src/main/java/events/dao/CategoryDAO.java
@@ -0,0 +1,5 @@
+package events.dao;
+
+public interface ModelDAO {
+
+}
---
+++ src/main/java/events/dao/impl/CategoryDAOImpl.java
@@ -0,0 +1,10 @@
+package events.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import events.dao.ModelDAO;
+
+@Repository
+public class ModelDAOImpl implements ModelDAO{
+
+}
