b10fc8806687dff9db91a5fdc1c1cdb4093dd008
info <info@PC1262.ebs.msft>
2017-08-07 14:23:05 +0300

    new packages

---
+++ src/main/java/events/service/impl/ModelServiceImpl.java
@@ -0,0 +1,10 @@
+package events.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import events.service.ModelService;
+
+@Service
+public class ModelServiceImpl implements ModelService{
+
+}
