9bc616dc47175bd811a2b89ac4d49ced331e1192
Zoli <Zoli@10.227.25.149>
2017-08-08 10:12:08 +0300

    Impl of service

---
+++ src/main/java/events/service/impl/FetchServiceImpl.java
@@ -0,0 +1,59 @@
+package events.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import events.dao.ArtistDAO;
+import events.dao.CategoryDAO;
+import events.dao.EventDAO;
+import events.dao.TicketDAO;
+import events.dao.UserDAO;
+import events.model.Artist;
+import events.model.Category;
+import events.model.Event;
+import events.model.Ticket;
+import events.model.User;
+import events.service.FetchService;
+
+@Service
+public class FetchServiceImpl implements FetchService {
+
+	@Autowired
+	private ArtistDAO artistDao;
+	
+	@Autowired
+	private CategoryDAO categoryDao;
+	
+	@Autowired
+	private EventDAO eventDao;
+	
+	@Autowired
+	private TicketDAO ticketDao;
+	
+	@Autowired
+	private UserDAO userDao;
+	
+	public List<Artist> getAllArtists() {
+		return artistDao.getAllArtists();
+	}
+
+	public List<Category> getAllCategories() {
+		// TODO Auto-generated method stub
+		return categoryDao.getAllCategories();
+	}
+
+	public List<Event> getAllEvents() {
+		return eventDao.getAllEvents();
+	}
+
+	public List<Ticket> getAllTickets() {
+		return ticketDao.getAllTickets();
+	}
+
+	public List<User> getAllUsers() {
+		return userDao.getAllUsers();
+	}
+
+}
