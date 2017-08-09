bb0ed0fae3ecd17f870a724de7e8b7141611c497
Zoli <Zoli@10.227.25.149>
2017-08-09 13:22:24 +0300

    Authentification

--- src/main/java/events/authentication/Authenticate.java
+++ src/main/java/events/authentication/Authenticate.java
@@ -1,5 +1,21 @@
 package events.authentication;
 
-public class Authenticate {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Authenticate implements AuthenticationProvider{
+
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean supports(Class<?> authentication) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
