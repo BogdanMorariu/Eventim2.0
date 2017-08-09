32fdb70db3c898b91c32c925cbc9a6b8bed683ba
Zoli <Zoli@10.227.25.149>
2017-08-09 10:42:00 +0300

    Artist Tests

--- src/main/java/events/model/Artist.java
+++ src/main/java/events/model/Artist.java
@@ -1,23 +1,31 @@
 package events.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Artist {
 	private Integer id;
 	private String name;
 	private List<Event> events;
-	
-	
 
-	
-	public List<Event> getEvents(){
-		return events;
+	public Artist() {
+
 	}
-	
-	public void setEvents(List<Event> events){
+
+	public Artist(Integer id, String name, List<Event> events) {
+		this.id = id;
+		this.name = name;
 		this.events = events;
 	}
-	
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -33,6 +41,12 @@ public class Artist {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
+	public void addEvent(Event event) {
+		if (events == null) {
+			events = new ArrayList<Event>();
+		}
+		events.add(event);
+	}
+
 }
