8ea3f066187ad72b83c6e5a4700135ae93ec898f
info <info@PC1255.ebs.msft>
2017-08-09 11:06:51 +0300

    Mail Send Test

---
+++ src/test/java/events/utils/MailsTest.java
@@ -0,0 +1,32 @@
+package events.utils;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+
+public class MailsTest {
+
+	public void testMail() throws MessagingException, IOException, InterruptedException  {
+		
+		
+		String body = "This is another test";
+		String senderMailAdress = "eventim2.0@gmail.com";
+		String recipientMailAdress = "geraldlionb@gmail.com";
+		
+		MailUtil.sendMail(body, senderMailAdress, recipientMailAdress);
+		
+		Thread.sleep(60000);
+
+	}
+	public static void main(String[] args) {
+		
+		String body = "Gerri";
+		String senderMailAdress = "eventim2.0@gmail.com";
+		String recipientMailAdress = "geraldlionb@gmail.com";
+		
+		MailUtil.sendMail(body, senderMailAdress, recipientMailAdress);
+		
+	}
+
+}
